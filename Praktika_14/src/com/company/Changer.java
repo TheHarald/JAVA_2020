0b6d41e6109b74e882e9aefd7730d380cd102e90
Roman Surinov <67330589+TheHarald@users.noreply.github.com>
2020-10-25 20:25:13 +0300

    upd pr14

--- Praktika_14/src/com/company/Changer.java
+++ Praktika_14/src/com/company/Changer.java
@@ -1,64 +1,70 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Changer {
-    private final String[] rules;
+
     private final String text;
     private int countRules;
+
+    ArrayList<Rules> rules = new ArrayList<>();
+
+    class Rules{
+        String first,second;
+        public Rules(String first, String second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
 
     public Changer() {
         Scanner scanner = new Scanner(System.in);
         countRules = Integer.parseInt(scanner.nextLine());
-        rules = new String[countRules];
-
         for (int i = 0; i < countRules; i++) {
-            rules[i]=scanner.nextLine();
+            rules.add(new Rules(scanner.next(),scanner.next()));
         }
-
-        text=scanner.next();
+        text = scanner.next();
     }
 
 
-    public void withRegex(){
-        String tempText=this.text;
-        Pattern pattern = Pattern.compile("(?<first>\\w+) (?<second>\\w+)");
-        Matcher matcher;
-
-        for (int i = 0; i < rules.length; i++) {
-            matcher = pattern.matcher(rules[i]);
-            if (matcher.find()) {
-                tempText = tempText.replaceAll(matcher.group("first"), " " + matcher.group("second") + " ");
-            }
-
+    public void withRegex() {
+        String tempText = text;
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < rules.size(); i++) {
+            stringBuilder.append(rules.get(i).first);
+            if(i!= rules.size() -1)
+                stringBuilder.append("|");
         }
 
-        System.out.println(" WithRegex : " + tempText.replace(" ", ""));
+        HashMap<String, String> replaceMap = new HashMap<String, String>();
+
+        for (int i = 0; i < rules.size(); i++)
+            replaceMap.put(rules.get(i).first,rules.get(i).second);
+
+
+        Pattern pattern = Pattern.compile(stringBuilder.toString());
+        Matcher matcher = pattern.matcher(tempText);
+        tempText= matcher.replaceAll(x -> replaceMap.get(x.group()));
+        System.out.println(" WithRegex : " + tempText);
     }
 
-    public void withoutRegex(){
 
-        class Rules{
-            private String first,second;
-
-            public Rules(String first, String second) {
-                this.first = first;
-                this.second = second;
+    public void withoutRegex() {
+        String tempText = text;
+            for (int i = 0; i < tempText.length(); i++) {
+                for (int j = 0; j < rules.size(); j++) {
+                    if (i + rules.get(j).first.length() <tempText.length() &&
+                            tempText.substring(i, i + rules.get(j).first.length()).equals(rules.get(j).first)) {
+                        tempText = tempText.replace(rules.get(j).first,rules.get(j).second + " ");
+                        i += rules.get(j).second.length();
+                        break;
+                    }
+                }
             }
+            System.out.println(" WithoutRegex : " + tempText.replaceAll(" ", ""));
         }
-
-        String tempText=text;
-        ArrayList<Rules> rules = new ArrayList<>();
-        for (int i = 0; i < countRules; i++){
-            rules.add(new Rules(this.rules[i].split(" ")[0],this.rules[i].split(" ")[1]));
-
-            if(tempText.indexOf(rules.get(i).first)!= -1){
-                tempText=tempText.replaceAll(rules.get(i).first," "+rules.get(i).second+" ");
-            }
-        }
-        System.out.println(" WithoutRegex : " + tempText.replace(" ",""));
-    }
 }
--- Praktika_14/src/com/company/Main.java
+++ Praktika_14/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Changer changer = new Changer();
 
-        //changer.withRegex();
+        changer.withRegex();
         changer.withoutRegex();
 
     }
